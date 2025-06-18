68b6a3d7480529c2b3b825b35ce06b0f2e66d279
nischal-z2 <nischal.aryal@orange.com>
2025-06-18 07:45:53 +0200

    Studied about comments

---
+++ comments/comments.rs
@@ -0,0 +1,18 @@
+// This is called a LINE COMMENT and is a regular comment
+
+/* 
+    This is called a BLOCK comment and is also a regular comment
+*/
+
+/// This is a document comment
+/// and its useful for explaining 
+/// the functions
+
+
+//! This is also a document comment
+//! and is generally used to describe
+//! a module
+//! Used at the top of the file
+
+
+/// /* */ this comment can be used between expressions as well.
